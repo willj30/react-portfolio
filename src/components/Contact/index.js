6d4aa52022d88c08b9596cdb52123b3bee8e7887
willj30 <willjaramillo30@gmail.com>
2023-04-24 00:36:01 -0500

    created contact form with functioning emailjs

---
+++ src/components/Contact/index.js
@@ -0,0 +1,106 @@
+import { useState } from 'react'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+import Loader from 'react-loaders'
+import { EmailJSResponseStatus } from '@emailjs/browser'
+import emailjs from '@emailjs/browser'
+import { useRef } from 'react'
+
+const Contact = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const refForm = useRef()
+
+    const sendEmail = (e) => {
+        e.preventDefault()
+    
+        emailjs
+          .sendForm('service_2tfy6po', 'template_n35cxms', refForm.current, 'CPtnCVVgXyn0TaiEP')
+          .then(
+            () => {
+              alert('Message successfully sent!')
+              window.location.reload(false)
+            },
+            () => {
+              alert('Oops! Soething went wrong, please try again')
+            }
+          )
+      }
+
+    return (
+        <>
+            <div className="container contact-page">
+                <div className="text-zone">
+                    <h1>
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={[
+                                'C',
+                                'o',
+                                'n',
+                                't',
+                                'a',
+                                'c',
+                                't',
+                                ' ',
+                                'M',
+                                'e',
+                            ]}
+                            idx={15}
+                        />
+                    </h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Quasi, voluptate.
+                    </p>
+                    <div className="contact-form">
+                        <form ref={refForm} onSubmit={sendEmail}>
+                            <ul>
+                                <li className="half">
+                                    <input
+                                        type="text"
+                                        name="name"
+                                        placeholder="Name"
+                                        required
+                                    />
+                                </li>
+                                <li className="half">
+                                    <input
+                                        type="email"
+                                        name="email"
+                                        placeholder="Email"
+                                        required
+                                    />
+                                </li>
+                                <li>
+                                    <input
+                                        placeholder="Subject"
+                                        type="text"
+                                        name="subject"
+                                        required
+                                    />
+                                </li>
+                                <li>
+                                    <textarea
+                                        placeholder="Message"
+                                        name="message"
+                                        required
+                                    ></textarea>
+                                </li>
+                                <li>
+                                    <input
+                                        type="submit"
+                                        className="flat-button"
+                                        value="SEND"
+                                    />
+                                </li>
+                            </ul>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <Loader type="cube-transition" />
+        </>
+    )
+}
+
+export default Contact
